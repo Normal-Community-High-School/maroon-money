9159d474a4472e691dd8746aaf4212461c6ed1f8
Nick Cooper <nicholascullencooper@gmail.com>
2022-12-04 20:29:32 -0600

    Dynamic import?

--- pages/loancalc.tsx
+++ pages/loancalc.tsx
@@ -1,17 +1,13 @@
-import { Title, Text, Box } from '@mantine/core';
-import Script from 'next/script'
+import dynamic from 'next/dynamic'
 
+const LoanCalcithNoSSR = dynamic(
+  () => import('../components/LoanCalc/LoanCalc'),
+  { ssr: false }
+)
 
 export default function LoanCalcPage() {
 
   return (
-    <div id="iq-calculators-embed-1669734975937">
-    <Script
-      type="text/javascript"
-      src="https://iqcalculators.com/calculator/student-loan/widget-v1.js?id=1669734975937&options=eyJib3JkZXIiOnRydWUsImNoYXJ0cyI6dHJ1ZSwidGl0bGUiOjAsImNvbG9ycyI6eyJwcmltYXJ5IjoiIzJhNTVhNiJ9fQ=="
-      strategy="beforeInteractive"
-      >
-    </Script>
-    </div>
+    <LoanCalcithNoSSR />
   );
 }
