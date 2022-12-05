058cecbb2d92ca176c47790a0f89bc38dc25c881
Nick Cooper <nicholascullencooper@gmail.com>
2022-12-04 20:57:12 -0600

    plain script?

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,11 @@
-import { Welcome } from '../components/Welcome/Welcome';
+import { Title, Text, Anchor } from '@mantine/core';
 
 export default function HomePage() {
   return (
     <>
-      <Welcome />
+      <Text color="dimmed" align="left" size="lg" mx="auto" mt="xl">
+        Hey! We are currently seniors at Normal Community High School creating a neat project. Our goal is to help high school seniors become prepared for college finances.
+      </Text>
     </>
   );
 }
--- pages/loancalc.tsx
+++ pages/loancalc.tsx
@@ -5,11 +5,11 @@ export default function LoanCalcPage() {
   return (
     <>
     <div id="iq-calculators-embed-1669734975937"> </div>
-    <Script
+    <script
       type="text/javascript"
       src="https://iqcalculators.com/calculator/student-loan/widget-v1.js?id=1669734975937&options=eyJib3JkZXIiOnRydWUsImNoYXJ0cyI6dHJ1ZSwidGl0bGUiOjAsImNvbG9ycyI6eyJwcmltYXJ5IjoiIzJhNTVhNiJ9fQ=="
       >
-    </Script>
+    </script>
     </>
   );
 }
