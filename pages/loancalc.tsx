7bbe63291500e20d8f3284042ef621ef88f70a46
Nick Cooper <nicholascullencooper@gmail.com>
2022-12-04 20:14:12 -0600

    box

--- pages/loancalc.tsx
+++ pages/loancalc.tsx
@@ -6,13 +6,15 @@ export default function LoanCalcPage() {
 
   return (
     <>
+    <Box id="iq-calculators-embed-1669734975937">
     <Script
-      id="iq-calculators-embed-1669734975937"
+      
       type="text/javascript"
       src="https://iqcalculators.com/calculator/student-loan/widget-v1.js?id=1669734975937&options=eyJib3JkZXIiOnRydWUsImNoYXJ0cyI6dHJ1ZSwidGl0bGUiOjAsImNvbG9ycyI6eyJwcmltYXJ5IjoiIzJhNTVhNiJ9fQ=="
       strategy="beforeInteractive"
       >
     </Script>
+    </Box>
     </>
   );
 }
